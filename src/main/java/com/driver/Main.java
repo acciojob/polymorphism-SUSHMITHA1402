fb174ecd14e86c676eb827540d273fdd9feac34a
SUSHMITHA1402 <gummadis999@gmail.com>
2023-01-13 20:21:07 +0530

    TRIY2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -8,16 +8,16 @@ public class Main {
         p.product(6.5,5.2);
 
     }
+    static class Product{
+        public int product(int x, int y) {
+            return x*y;
+        }
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+        public double product(double x, double y){
+            return x*y;
+        }
+    }
 }
 
-class Product{
-    public int product(int x, int y) {
-        return x*y;
-    }
-    public int product(int x, int y, int z) {
-        return x*y*z;
-    }
-    public double product(double x, double y){
-        return x*y;
-    }
-}
